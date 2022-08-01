fa8837d98ced5851df7d263734f5c8134e3ee5e4
aml-youssef <aml.tota2001@gmail.com>
2022-08-01 10:08:56 +0200

    themes

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,51 @@ class ToDoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: BoardScreen(),
       routes: {
         AddTaskScreen.routName: (cxt) => AddTaskScreen(),
         ScheduleScreen.RoutName: (cxt) => ScheduleScreen(),
       },
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black //here you can give the text color
+            ),
+        tabBarTheme: const TabBarTheme(
+          labelColor: Colors.black,
+        ),
+        // primaryTextTheme: TextTheme(headline6: TextStyle(color: Colors.red)),
+
+        // colorScheme: const ColorScheme.light()
+        //     .copyWith(primary: Colors.white10, secondary: Colors.yellow,),
+        inputDecorationTheme: const InputDecorationTheme(
+          fillColor: Color.fromARGB(255, 230, 230, 230),
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+        ),
+        //primarySwatch: Colors.white,
+        toggleableActiveColor: const Color.fromARGB(255, 238, 34, 34),
+        splashColor: Colors.yellow,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          primary: const Color.fromARGB(255, 39, 240, 143),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        )),
+      ),
     );
   }
 }
